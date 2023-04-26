6fdc84c0b0e4d8bd38cf1ccacdcbe7672df86e56
zzberries <zzbits10@gmail.com>
2023-04-25 20:44:33 -0400

    formats map and image

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,21 +5,13 @@
 import FlutterMacOS
 import Foundation
 
-<<<<<<< Updated upstream
 import cloud_firestore
 import firebase_core
 import path_provider_foundation
-=======
-import firebase_core
-import path_provider_macos
->>>>>>> Stashed changes
 import sqflite
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-<<<<<<< Updated upstream
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
-=======
->>>>>>> Stashed changes
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
